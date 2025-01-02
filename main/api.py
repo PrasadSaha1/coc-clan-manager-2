9036b7b099c7bcac2994be4250010baa396512da
PrasadSaha1 <prasadsaha11@gmail.com>
2025-01-02 16:01:34 -0500

    jhgc

--- main/api.py
+++ main/api.py
@@ -5,14 +5,14 @@ headers = {
     "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjkwOWMxNmU3LTIwOTYtNDEzMy05ODAwLTk4ZjRjOWFlY2NlNCIsImlhdCI6MTczMTE4NTQ5Miwic3ViIjoiZGV2ZWxvcGVyLzYwYjgyNGZhLTBhYjUtZjZhOC04Zjk1LTFkZTY5YTVlYWFlNSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjY3LjgzLjIwMi4xMzAiXSwidHlwZSI6ImNsaWVudCJ9XX0.3P1pnIYig0c_css_lu2GQ7sJ2uICw-7ysoCsZ3l7_vgibkFxRGSNuSjsfp0ghwaAVtqfXKmw9VMGEyfT9JS6lw"
 }
 
-def find_clan_with_tag(clan_tag, information):
+def find_clan_with_tag(clan_tag, information=None):
     response = requests.get(f"https://api.clashofclans.com/v1/clans/%23{clan_tag}", headers=headers)
     response_json = response.json()
 
-    information_to_export = []
-    for info in information:
-        information_to_export.append(response_json[info])
-    return information_to_export
+    #information_to_export = []
+    #for info in information:
+     #   information_to_export.append(response_json[info])
+    return response_json
 
 def get_clan_badge(clan_tag):
     response = requests.get(f"https://api.clashofclans.com/v1/clans/%23{clan_tag}", headers=headers)
