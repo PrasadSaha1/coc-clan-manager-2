2c1f592e3b91811da7aee93d36ca19a72be3aaf1
PrasadSaha1 <prasadsaha11@gmail.com>
2025-01-02 16:26:22 -0500

    fdgsdfgfs

--- main/api.py
+++ main/api.py
@@ -2,20 +2,20 @@ import requests
 from main.models import *
 
 headers = {
-    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImViNWYyM2ViLWFiMTUtNGVhMi1iMTkwLTY4YzI5ZmVmNzdiNSIsImlhdCI6MTczNTg1MTkwMSwic3ViIjoiZGV2ZWxvcGVyLzYwYjgyNGZhLTBhYjUtZjZhOC04Zjk1LTFkZTY5YTVlYWFlNSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE4LjIwNi4xNjUuMTQyIiwiNjcuODMuMjAyLjEzMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.egshJnhxYrlNOKVqVgcxMrGiSQ_ZaKDmDHoers6UwDildhazAAZX4xdEH_DtlJjoTwHCvUUd7GahWY050W2IRA"
+    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE0YmQyNzcyLTBjOWMtNGRlZi04MTVhLTliNDI4MDVlZjliMiIsImlhdCI6MTczNTg1MjcwMSwic3ViIjoiZGV2ZWxvcGVyLzYwYjgyNGZhLTBhYjUtZjZhOC04Zjk1LTFkZTY5YTVlYWFlNSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1Ljc5LjIxOC43OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.OrnUTTTl4P6ctN7yqpqBLrrLORI1J7d0tuGgWNdxyzUtyIwK6TFYB0Up68iGhZu6zFMmzwoGPsLOQ8tF76HooQ"
 }
 
-def find_clan_with_tag(clan_tag, information=[]):
-    response = requests.get(f"https://api.clashofclans.com/v1/clans/%23{clan_tag}", headers=headers)
+def find_clan_with_tag(clan_tag, information):
+    response = requests.get(f"https://cocproxy.royaleapi.dev/v1/clans/%23{clan_tag}", headers=headers)
     response_json = response.json()
 
     information_to_export = []
     for info in information:
         information_to_export.append(response_json[info])
-    return response_json
+    return information_to_export
 
 def get_clan_badge(clan_tag):
-    response = requests.get(f"https://api.clashofclans.com/v1/clans/%23{clan_tag}", headers=headers)
+    response = requests.get(f"https://cocproxy.royaleapi.dev/v1/clans/%23{clan_tag}", headers=headers)
     response_json = response.json()
     return response_json["badgeUrls"]["medium"]
 
