410991ecd74c0109d6afb6100d21765978c331f8
PrasadSaha1 <prasadsaha11@gmail.com>
2025-04-17 18:29:44 -0400

    Static Files

--- COC_app/settings.py
+++ COC_app/settings.py
@@ -189,6 +189,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # or os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
